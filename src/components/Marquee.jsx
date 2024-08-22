478d6ab263834e274a168a59db5873eec483cbac
AnzalnaKainat <anzalnakainat@gmail.com>
2024-08-22 19:50:23 +0500

    responsiveness

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -7,8 +7,8 @@ const Marquee = () => {
 
     <div data-scroll data-scroll-section data-scroll-speed=".01" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
         <div className='text text-white border-t-[1px] border-b-[1px] border-zinc-400 flex overflow-hidden whitespace-nowrap'>
-            <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{repeat: Infinity, ease: "linear", duration: 10}} className='text-[15vw] leading-none font-["Founder_Grotesk_X-Condensed"] uppercase pt-10 -mt-10 font-semibold pr-20'>We are ochi</motion.h1>
-            <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{repeat: Infinity, ease: "linear", duration: 10}} className='text-[15vw] leading-none font-["Founder_Grotesk_X-Condensed"] uppercase pt-10 -mt-10 font-semibold pr-20'>We are ochi</motion.h1>
+            <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{repeat: Infinity, ease: "linear", duration: 10}} className='text-[15vw] leading-none font-["Founder_Grotesk_X-Condensed"] uppercase pt-10 -mt-10 font-semibold sm:pr-10 pr-5'>We are ochi</motion.h1>
+            <motion.h1 initial={{x: "0"}} animate={{x: "-100%"}} transition={{repeat: Infinity, ease: "linear", duration: 10}} className='text-[15vw] leading-none font-["Founder_Grotesk_X-Condensed"] uppercase pt-10 -mt-10 font-semibold sm:pr-10 pr-5'>We are ochi</motion.h1>
         </div>
     </div>
   )
